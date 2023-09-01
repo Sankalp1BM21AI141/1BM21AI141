e517af54f162276451ce20fc3f1bb332b493fe10
Sankalp1BM21AI141 <139086310+Sankalp1BM21AI141@users.noreply.github.com>
2023-09-01 09:53:44 +0530

    Create floyd.cpp

---
+++ floyd.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+#define nV 4
+
+#define INF 999
+class floy{
+    public:
+    int graph[nV][nV] = {{0, 3, INF, 5},
+             {2, 0, INF, 4},
+             {INF, 1, 0, INF},
+             {INF, INF, 2, 0}};
+    void floydWarshall(int graph[][nV]);
+    void printMatrix(int matrix[][nV]);
+    
+    
+}flo;
+
+
+
+
+void floy::floydWarshall(int graph[][nV]) {
+  int matrix[nV][nV], i, j, k;
+
+  for (i = 0; i < nV; i++)
+    for (j = 0; j < nV; j++)
+      matrix[i][j] = graph[i][j];
+
+  
+  for (k = 0; k < nV; k++) {
+    for (i = 0; i < nV; i++) {
+      for (j = 0; j < nV; j++) {
+        if (matrix[i][k] + matrix[k][j] < matrix[i][j])
+          matrix[i][j] = matrix[i][k] + matrix[k][j];
+      }
+    }
+  }
+  printMatrix(matrix);
+}
+
+void floy:: printMatrix(int matrix[][nV]) {
+  for (int i = 0; i < nV; i++) {
+    for (int j = 0; j < nV; j++) {
+      if (matrix[i][j] == INF)
+        printf("%4s", "INF");
+      else
+        printf("%4d", matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int main() {
+  
+  flo.floydWarshall(flo.graph);
+}
