e6c645292d17f2c7faa788a6d8bfd1822b154213
Sankalp1BM21AI141 <139086310+Sankalp1BM21AI141@users.noreply.github.com>
2023-09-04 23:32:06 +0530

    Update harspool.cpp

--- harspool.cpp
+++ harspool.cpp
@@ -1,70 +1,65 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
 const int ALPHABET_SIZE = 256;
+class hars{
+    public:
+    string text = "BMS college of engineering";
+    string pattern = "college";
+    void precomputeBadCharacterTable(const string& pattern, vector<int>& badCharTable);
+    int horspoolSearch(const string& text, const string& pattern);
+    
+    
+    
+}ha;
 
-// To generate shift table
-void generateShiftTable(const string& pattern, int shiftTable[]) {
-    int m = pattern.length();
-
-    // Initialising the shift table
-    for (int i = 0; i < ALPHABET_SIZE; ++i) {
-        shiftTable[i] = m;
+// Preprocess the pattern and create the bad character table
+void hars:: precomputeBadCharacterTable(const string& pattern, vector<int>& badCharTable) {
+    int patternLength = pattern.length();
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        badCharTable[i] = patternLength; // Initialize with the pattern length as default value
     }
-
-    // Fill in the shift values
-    for (int i = 0; i < m - 1; ++i) {
-        shiftTable[pattern[i]] = m - 1 - i;
+    for (int i = 0; i < patternLength - 1; i++) {
+        badCharTable[pattern[i]] = patternLength - 1 - i;
     }
 }
 
-// string search function
-int horspoolSearch(const string& text, const string& pattern) {
-    int n = text.length();
-    int m = pattern.length();
-
-    if (n < m) {
-        return -1; // no match
+// Horspool string searching algorithm
+int hars:: horspoolSearch(const string& text, const string& pattern) {
+    int textLength = text.length();
+    int patternLength = pattern.length();
+    if (patternLength > textLength) {
+        return -1; // Pattern is longer than the text, no match possible
     }
 
-    int shiftTable[ALPHABET_SIZE];
-    generateShiftTable(pattern, shiftTable);
+    vector<int> badCharTable(ALPHABET_SIZE);
+    precomputeBadCharacterTable(pattern, badCharTable);
 
-    int i = m - 1;
-    int j;
-
-    while (i < n) {
-        j = m - 1;
-
-        while (j >= 0 && text[i] == pattern[j]) {
-            --i;
-            --j;
+    int i = patternLength - 1; // Start from the rightmost character of the pattern
+    while (i < textLength) {
+        int j = 0;
+        while (j < patternLength && text[i - j] == pattern[patternLength - 1 - j]) {
+            j++;
         }
-
-        if (j == -1) {
-            return i + 1; // Match found
+        if (j == patternLength) {
+            return i - patternLength + 1; // Pattern found at position i - patternLength + 1
         }
-
-        i += shiftTable[text[i]];
+        i += badCharTable[text[i]];
     }
 
-    return -1; // No match found
+    return -1; // Pattern not found in the text
 }
 
 int main() {
-    string text = "Betty got some butter but butter was bitter";
-    string pattern = "bitter";
+    
 
-    cout<<"Text in which the pattern has to be matched:"<<text<<endl;
-    cout<<"The pattern to be matched: "<<pattern<<endl;
-
-    int result = horspoolSearch(text, pattern);
+    int result = ha.horspoolSearch(ha.text, ha.pattern);
 
     if (result != -1) {
-        cout << "Pattern found at index " << result << endl;
+        cout << "Pattern found at position " << result << endl;
     } else {
         cout << "Pattern not found in the text." << endl;
     }
