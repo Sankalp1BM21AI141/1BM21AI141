063303e2f43136b2792295299ef4c9ea0c33faa4
Sankalp1BM21AI141 <139086310+Sankalp1BM21AI141@users.noreply.github.com>
2023-09-05 10:17:39 +0530

    Update topologicalsort.cpp

--- topologicalsort.cpp
+++ topologicalsort.cpp
@@ -1,52 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+
 using namespace std;
+
 class Graph {
 private:
-    int V; // Number of vertices
-    vector<vector<int>> adj; // Adjacency list
+    int V;  
+    vector<vector<int>> adj; 
 
 public:
-    Graph(int v) : V(v), adj(v) {}
+    Graph(int vertices) : V(vertices), adj(vertices) {}
 
-    void addEdge(int v, int w) {
-        adj[v].push_back(w);
+   
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
     }
 
-    void topologicalSortUtil(int v, vector<bool>& visited, stack<int>& stack) {
+
+    void topologicalSortDFS(int v, vector<bool>& visited, stack<int>& result) {
         visited[v] = true;
 
-        for (int neighbor : adj[v]) {
+        for (const int& neighbor : adj[v]) {
             if (!visited[neighbor]) {
-                topologicalSortUtil(neighbor, visited, stack);
+                topologicalSortDFS(neighbor, visited, result);
             }
         }
 
-        stack.push(v);
+        result.push(v);
     }
 
+  
     void topologicalSort() {
         vector<bool> visited(V, false);
-        stack<int> stack;
+        stack<int> result;
 
         for (int i = 0; i < V; ++i) {
             if (!visited[i]) {
-                topologicalSortUtil(i, visited, stack);
+                topologicalSortDFS(i, visited, result);
             }
         }
 
-        cout << "Topological Sorting: ";
-        while (!stack.empty()) {
-            cout << stack.top() << " ";
-            stack.pop();
+        // Print the topological ordering
+        cout << "Topological Ordering:" << endl;
+        while (!result.empty()) {
+            cout << result.top() << " ";
+            result.pop();
         }
+        cout << endl;
     }
 };
 
 int main() {
-    Graph g(6);
+    Graph g(6); // Create a graph with 6 vertices
 
+    // Add directed edges
     g.addEdge(5, 2);
     g.addEdge(5, 0);
     g.addEdge(4, 0);
@@ -54,7 +62,6 @@ int main() {
     g.addEdge(2, 3);
     g.addEdge(3, 1);
 
-    cout << "Topological Sort order: ";
     g.topologicalSort();
 
     return 0;
